8c329c15d032797b15f00d2ca392df7c95a4178d
work-michael-temp <work.mtapia@gmail.com>
2025-01-24 19:56:55 +0000

    Configured vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+  base: '/valentines-with-me/', // For project site
+  // For user site, use: base: '/'
+  build: {
+    outDir: 'dist',
   },
-})
+});
